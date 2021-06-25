1738d5cda9efba093a5558968e1799e4fcd7b3c5
Larry Marzan Jr <Larry.MarzanJr@gmail.com>
2021-06-25 13:29:22 +0900

    import pywhatkit and test send message

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
 import pywhatkit
 
-# set array of phone numbers (test)
-phone_numbers = ['+6281242181136','+6285398916361','+6282196660447',]
-
-pywhatkit.sendwhatmsg_instantly("+6282196660447", "[Ini adalah pesan otomatis] Pelanggan JR-Network Yth. saat ini tagihan Internet bulan ini sudah ada. Terima kasih")
+pywhatkit.sendwhatmsg_instantly("+6281234567890", "[Ini adalah pesan otomatis] Pelanggan JR-Network Yth. saat ini tagihan Internet bulan ini sudah ada. Terima kasih")
