d34ba93d0c6addf7a0000f92cf5ae6cafaecaed8
Larry Marzan Jr <Larry.MarzanJr@gmail.com>
2021-06-25 12:18:53 +0900

    sending static instant message (without database)

---
+++ main.py
@@ -0,0 +1,6 @@
+import pywhatkit
+
+# set array of phone numbers (test)
+phone_numbers = ['+6281242181136','+6285398916361','+6282196660447',]
+
+pywhatkit.sendwhatmsg_instantly("+6282196660447", "[Ini adalah pesan otomatis] Pelanggan JR-Network Yth. saat ini tagihan Internet bulan ini sudah ada. Terima kasih")
