164302641805c3232ae2d6ab389b93d05eec11fb
Larry Marzan Jr <Larry.MarzanJr@gmail.com>
2021-06-26 22:23:50 +0800

    unused file

--- main.py
+++
@@ -1,3 +0,0 @@
-import pywhatkit
-
-pywhatkit.sendwhatmsg_instantly("+6281234567890", "[Ini adalah pesan otomatis] Pelanggan JR-Network Yth. saat ini tagihan Internet bulan ini sudah ada. Terima kasih")
